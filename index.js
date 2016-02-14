3fcb20672f7113472b812a202636031f76214c6d
Nisheeth Kashyap <nisheeth.k.kashyap@gmail.com>
2016-02-14 19:39:51 +0000

    cleanup and remove promises for sync calls

--- index.js
+++ index.js
@@ -3,12 +3,33 @@
  * Created by Nisheeth on 06/02/2016.
  */
 
+/**
+ * Create a new ChildProcess
+ * @class
+ * @name ChildProcess
+ * @see {@link https://nodejs.org/api/child_process.html}
+ */
 class ChildProcess {
 
     constructor() {
         this.child_process = require('child_process');
     }
 
+    /**
+     * @method
+     * @param {string} command - The command to run, with space-separated arguments.
+     * @param {object} [options] - command options.
+     * @param {string} options.cwd - Current working directory of the child process.
+     * @param {object} options.env - Environment key-value pairs
+     * @param {string} options.encoding - (Default: 'utf8')
+     * @param {string} options.shell - Shell to execute the command with (Default: '/bin/sh' on UNIX, 'cmd.exe' on Windows, The shell should understand the -c switch on UNIX or /s /c on Windows. On Windows, command line parsing should be compatible with cmd.exe.)
+     * @param {number} options.timeout - (Default: 0)
+     * @param {number} options.maxBuffer - Number largest amount of data (in bytes) allowed on stdout or stderr - if exceeded child process is killed (Default: 200*1024).
+     * @param {string} options.killSignal - (Default: 'SIGTERM').
+     * @param {number} options.uid - Sets the user identity of the process.
+     * @param {number} options.gid - Sets the group identity of the process.
+     * @return {Promise} A Promise object.
+     */
     exec(command, options) {
         let child;
         let promise = new Promise((resolve, reject) => {
@@ -25,6 +46,21 @@ class ChildProcess {
         return promise;
     }
 
+    /**
+     * @method
+     * @param {string} file - A path to an executable file
+     * @param {array} [args] - List of string arguments
+     * @param {object} [options] - command options.
+     * @param {string} options.cwd - Current working directory of the child process.
+     * @param {object} options.env - Environment key-value pairs
+     * @param {string} options.encoding - (Default: 'utf8')
+     * @param {number} options.timeout - (Default: 0)
+     * @param {number} options.maxBuffer - Number largest amount of data (in bytes) allowed on stdout or stderr - if exceeded child process is killed (Default: 200*1024).
+     * @param {string} options.killSignal - (Default: 'SIGTERM').
+     * @param {number} options.uid - Sets the user identity of the process.
+     * @param {number} options.gid - Sets the group identity of the process.
+     * @return {Promise} A Promise object.
+     */
     execFile(file, args, options) {
         let child;
         let promise = new Promise((resolve, reject) => {
@@ -41,6 +77,20 @@ class ChildProcess {
         return promise;
     }
 
+    /**
+     * @method
+     * @param {string} modulePath - The module to run in the child
+     * @param {array} [args] - List of string arguments
+     * @param {object} [options] - command options.
+     * @param {string} options.cwd - Current working directory of the child process.
+     * @param {object} options.env - Environment key-value pairs
+     * @param {string} options.execPath - Executable used to create the child process
+     * @param {array} options.execArgv - List of string arguments passed to the executable (Default: process.execArgv)
+     * @param {boolean} options.silent - If true, stdin, stdout, and stderr of the child will be piped to the parent, otherwise they will be inherited from the parent, see the 'pipe' and 'inherit' options for child_process.spawn()'s stdio for more details (default is false)
+     * @param {number} options.uid - Sets the user identity of the process.
+     * @param {number} options.gid - Sets the group identity of the process.
+     * @return {Promise} A Promise object.
+     */
     fork(modulePath, args, options) {
         let child;
         let promise = new Promise((resolve, reject) => {
@@ -71,6 +121,19 @@ class ChildProcess {
         return promise;
     }
 
+    /**
+     * @method
+     * @param {string} command - The command to run.
+     * @param {array} [args] - List of string arguments
+     * @param {object} [options] - command options.
+     * @param {string} options.cwd - Current working directory of the child process.
+     * @param {object} options.env - Environment key-value pairs
+     * @param {array|string} options.stdio - Child's stdio configuration. (See options.stdio)
+     * @param {boolean} options.detached - Prepare child to run independently of its parent process. Specific behavior depends on the platform, see options.detached)
+     * @param {number} options.uid - Sets the user identity of the process.
+     * @param {number} options.gid - Sets the group identity of the process.
+     * @return {Promise} A Promise object.
+     */
     spawn(command, args, options) {
         let child;
         let promise = new Promise((resolve, reject) => {
@@ -101,42 +164,98 @@ class ChildProcess {
         return promise;
     }
 
+    /**
+     * @method
+     * @param {string} command - The command to run, with space-separated arguments.
+     * @param {object} [options] - command options.
+     * @param {string} options.cwd - Current working directory of the child process.
+     * @param {object} options.env - Environment key-value pairs
+     * @param {array|string} options.input -  The value which will be passed as stdin to the spawned process, supplying this value will override stdio[0]
+     * @param {array} options.stdio - Child's stdio configuration. (Default: 'pipe'), stderr by default will be output to the parent process' stderr unless stdio is specified
+     * @param {string} options.encoding - The encoding used for all stdio inputs and outputs. (Default: 'buffer')
+     * @param {string} options.shell - Shell to execute the command with (Default: '/bin/sh' on UNIX, 'cmd.exe' on Windows, The shell should understand the -c switch on UNIX or /s /c on Windows. On Windows, command line parsing should be compatible with cmd.exe.)
+     * @param {number} options.timeout - In milliseconds the maximum amount of time the process is allowed to run. (Default: undefined)
+     * @param {number} options.maxBuffer - Number largest amount of data (in bytes) allowed on stdout or stderr - if exceeded child process is killed
+     * @param {string} options.killSignal - The signal value to be used when the spawned process will be killed. (Default: 'SIGTERM')
+     * @param {number} options.uid - Sets the user identity of the process.
+     * @param {number} options.gid - Sets the group identity of the process.
+     * @return {buffer|string} The stdout from the command.
+     */
     execSync(command, options) {
-        options = options || {};
-        options.encoding = 'utf8';
-        return (new Promise((resolve, reject) => {
-            try {
-                resolve({stdout: this.child_process.execSync(command, options)});
-            } catch (error) {
-                error.code = error.status;
-                reject(error);
-            }
-        }));
+        return this.child_process.execSync.apply(this.child_process, arguments);
+        //options = options || {};
+        //options.encoding = 'utf8';
+        //return (new Promise((resolve, reject) => {
+        //    try {
+        //        resolve({stdout: this.child_process.execSync(command, options)});
+        //    } catch (error) {
+        //        error.code = error.status;
+        //        reject(error);
+        //    }
+        //}));
     }
 
+    /**
+     * @method
+     * @param {string} file - The filename of the program to run
+     * @param {array} [args] - List of string arguments
+     * @param {object} [options] - command options.
+     * @param {string} options.cwd - Current working directory of the child process.
+     * @param {object} options.env - Environment key-value pairs
+     * @param {array|string} options.input -  The value which will be passed as stdin to the spawned process, supplying this value will override stdio[0]
+     * @param {array} options.stdio - Child's stdio configuration. (Default: 'pipe'), stderr by default will be output to the parent process' stderr unless stdio is specified
+     * @param {string} options.encoding - The encoding used for all stdio inputs and outputs. (Default: 'buffer')
+     * @param {string} options.shell - Shell to execute the command with (Default: '/bin/sh' on UNIX, 'cmd.exe' on Windows, The shell should understand the -c switch on UNIX or /s /c on Windows. On Windows, command line parsing should be compatible with cmd.exe.)
+     * @param {number} options.timeout - In milliseconds the maximum amount of time the process is allowed to run. (Default: undefined)
+     * @param {number} options.maxBuffer - Number largest amount of data (in bytes) allowed on stdout or stderr - if exceeded child process is killed
+     * @param {string} options.killSignal - The signal value to be used when the spawned process will be killed. (Default: 'SIGTERM')
+     * @param {number} options.uid - Sets the user identity of the process.
+     * @param {number} options.gid - Sets the group identity of the process.
+     * @return {buffer|string} The stdout from the command.
+     */
     execFileSync(file, args, options) {
-        options = options || {};
-        options.encoding = 'utf8';
-        return (new Promise((resolve, reject) => {
-            try {
-                resolve({stdout: this.child_process.execFileSync(file, args, options).toString()});
-            } catch (error) {
-                reject(error);
-            }
-        }));
+        return this.child_process.execFileSync.apply(this.child_process, arguments);
+        //options = options || {};
+        //options.encoding = 'utf8';
+        //return (new Promise((resolve, reject) => {
+        //    try {
+        //        resolve({stdout: this.child_process.execFileSync(file, args, options).toString()});
+        //    } catch (error) {
+        //        reject(error);
+        //    }
+        //}));
     }
 
+    /**
+     * @method
+     * @param {string} command - The command to run, with space-separated arguments.
+     * @param {array} [args] - List of string arguments
+     * @param {object} [options] - command options.
+     * @param {string} options.cwd - Current working directory of the child process.
+     * @param {object} options.env - Environment key-value pairs
+     * @param {array|string} options.input -  The value which will be passed as stdin to the spawned process, supplying this value will override stdio[0]
+     * @param {array} options.stdio - Child's stdio configuration. (Default: 'pipe'), stderr by default will be output to the parent process' stderr unless stdio is specified
+     * @param {string} options.encoding - The encoding used for all stdio inputs and outputs. (Default: 'buffer')
+     * @param {string} options.shell - Shell to execute the command with (Default: '/bin/sh' on UNIX, 'cmd.exe' on Windows, The shell should understand the -c switch on UNIX or /s /c on Windows. On Windows, command line parsing should be compatible with cmd.exe.)
+     * @param {number} options.timeout - In milliseconds the maximum amount of time the process is allowed to run. (Default: undefined)
+     * @param {number} options.maxBuffer - Number largest amount of data (in bytes) allowed on stdout or stderr - if exceeded child process is killed
+     * @param {string} options.killSignal - The signal value to be used when the spawned process will be killed. (Default: 'SIGTERM')
+     * @param {number} options.uid - Sets the user identity of the process.
+     * @param {number} options.gid - Sets the group identity of the process.
+     * @return {object} The result object.
+     */
     spawnSync(command, args, options) {
-        options = options || {};
-        options.encoding = 'utf8';
-        return (new Promise((resolve, reject) => {
-            let result = this.child_process.spawnSync(command, args, options);
-            if (result.error) {
-                result.error.stderr = result.stderr;
-                return reject(result.error);
-            }
-            resolve({code: result.status, signal: result.signal, stdout: result.stdout});
-        }));
+        return this.child_process.spawnSync.apply(this.child_process, arguments);
+        //options = options || {};
+        //options.encoding = 'utf8';
+        //return (new Promise((resolve, reject) => {
+        //    let result = this.child_process.spawnSync(command, args, options);
+        //    if (result.error) {
+        //        result.error.stderr = result.stderr;
+        //        return reject(result.error);
+        //    }
+        //    resolve({code: result.status, signal: result.signal, stdout: result.stdout});
+        //}));
     }
 }
 
--- test.js
+++ test.js
@@ -75,64 +75,64 @@ describe('child-process-es6-promise', () => {
         });
     });
 
-    describe('execSync()', () => {
-        it('should execute command with correct arguments', (done) => {
-            cp.execSync('echo test')
-                .then((result)=> {
-                    assert.equal(result.stdout, 'test\n');
-                    done();
-                });
-        });
-        it('should fail to execute command ', (done) => {
-            cp.execSync('eco test')
-                .catch((error) => {
-                    assert.equal(error.code, 127);
-                    assert.equal(error.signal, null);
-                    assert.equal(error.cmd, 'eco test');
-                    assert.equal(error.stderr, '/bin/sh: 1: eco: not found\n');
-                    done();
-                });
-        });
-    });
-
-    describe('execFileSync()', () => {
-        it('should execute command with correct arguments', (done) => {
-            cp.execFileSync('echo', ['test'])
-                .then((result)=> {
-                    assert.equal(result.stdout, 'test\n');
-                    done();
-                });
-        });
-        it('should fail to execute command ', (done) => {
-            cp.execFileSync('eco', ['test'])
-                .catch((error) => {
-                    assert.equal(error.toString(), 'TypeError: invalid data');
-                    done();
-                });
-        });
-    });
-
-    describe('spawnSync()', () => {
-        it('should execute command with correct arguments', (done) => {
-            cp.spawnSync('echo', ['test'])
-                .then((result)=> {
-                    assert.equal(result.code, 0);
-                    assert.equal(result.signal, null);
-                    assert.equal(result.stdout, 'test\n');
-                    done();
-                });
-        });
-        it('should fail to execute command ', (done) => {
-            cp.spawnSync('eco', ['test'])
-                .catch((error) => {
-                    assert.equal(error.code, 'ENOENT');
-                    assert.equal(error.errno, 'ENOENT');
-                    assert.equal(error.syscall, 'spawnSync eco');
-                    assert.equal(error.path, 'eco');
-                    assert.equal(error.stderr, null);
-                    done();
-                });
-        });
-    });
+    //describe('execSync()', () => {
+    //    it('should execute command with correct arguments', (done) => {
+    //        cp.execSync('echo test')
+    //            .then((result)=> {
+    //                assert.equal(result.stdout, 'test\n');
+    //                done();
+    //    });
+    //    });
+    //    it('should fail to execute command ', (done) => {
+    //        cp.execSync('eco test')
+    //            .catch((error) => {
+    //        assert.equal(error.code, 127);
+    //        assert.equal(error.signal, null);
+    //        assert.equal(error.cmd, 'eco test');
+    //        assert.equal(error.stderr, '/bin/sh: 1: eco: not found\n');
+    //                done();
+    //            });
+    //    });
+    //});
+    //
+    //describe('execFileSync()', () => {
+    //    it('should execute command with correct arguments', (done) => {
+    //        cp.execFileSync('echo', ['test'])
+    //            .then((result)=> {
+    //                assert.equal(result.stdout, 'test\n');
+    //                done();
+    //            });
+    //    });
+    //    it('should fail to execute command ', (done) => {
+    //        cp.execFileSync('eco', ['test'])
+    //            .catch((error) => {
+    //                assert.equal(error.toString(), 'TypeError: invalid data');
+    //                done();
+    //            });
+    //    });
+    //});
+    //
+    //describe('spawnSync()', () => {
+    //    it('should execute command with correct arguments', (done) => {
+    //        cp.spawnSync('echo', ['test'])
+    //            .then((result)=> {
+    //                assert.equal(result.code, 0);
+    //                assert.equal(result.signal, null);
+    //                assert.equal(result.stdout, 'test\n');
+    //                done();
+    //            });
+    //    });
+    //    it('should fail to execute command ', (done) => {
+    //        cp.spawnSync('eco', ['test'])
+    //            .catch((error) => {
+    //                assert.equal(error.code, 'ENOENT');
+    //                assert.equal(error.errno, 'ENOENT');
+    //                assert.equal(error.syscall, 'spawnSync eco');
+    //                assert.equal(error.path, 'eco');
+    //                assert.equal(error.stderr, null);
+    //                done();
+    //            });
+    //    });
+    //});
 
 });
