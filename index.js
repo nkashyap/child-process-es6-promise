2d6dc453358a3a45d5810f89e81b54c59d094de8
Borek Bernard <borekb@gmail.com>
2016-11-08 15:07:43 +0100

    Added 'stderr' to fork result, similarly to 65039a982b918f79e46f9c24c6173c0dfaa79035

--- index.js
+++ index.js
@@ -99,7 +99,7 @@ class ChildProcess {
             child = this.child_process
                 .fork(modulePath, args, options)
                 .on('close', (code, signal) => {
-                    resolve({code: code, signal: signal, stdout: stdout});
+                    resolve({code: code, signal: signal, stdout: stdout, stderr: stderr});
                 })
                 .on('error', (error) => {
                     error.stderr = stderr;
